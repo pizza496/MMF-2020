28260e11870de8598939e6ff7e2134f48f3adb64
pizza496 <30300031+pizza496@users.noreply.github.com>
2020-01-27 19:01:37 -0800

    menus perfectd, mathjax added to math page, links corrected

--- Backend Generators/latexTranslator.py
+++ Backend Generators/latexTranslator.py
@@ -1,6 +1,6 @@
 import latex2mathml.converter
 
-latex_input = r"\begin{matrix*}[r]a & b \\ c & d \end{matrix*}"
+latex_input = r"\left[\begin{matrix} a & b \\ c & d \end{matrix}\right]"
 
 mathml_output = latex2mathml.converter.convert(latex_input)
 
