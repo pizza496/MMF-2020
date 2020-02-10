760614e39ff4444f00f0d90c9a95c51f14612f72
pizza496 <30300031+pizza496@users.noreply.github.com>
2020-02-10 12:36:29 -0800

    Reorganization
    reorganized to better support a webpage

---
+++ explanation/coding_files/example_template.js
@@ -0,0 +1,8 @@
+var viewbox_width = 1920
+var N = 5000 // number of input samples
+var M = 300 // number of circles
+var q = 20000 // number of output samples
+
+
+let P = //After this point is an array of complex numbers
+//the array itself looks like [[re1, im1], [re2, im2], [re3, im3] ... ]
